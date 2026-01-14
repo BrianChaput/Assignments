4cac3a1db0a6a7efec1edec138d418521d836c9b
Brian Chaput <113158385+BrianChaput@users.noreply.github.com>
2026-01-13 20:32:40 -0800

    Add crypt.py for cryptographic operations

---
+++ secure_chat/lib/crypt.py
@@ -0,0 +1,15 @@
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms
+
+from cryptography.hazmat.backends import default_backend
+
+from cryptography.hazmat.primitives import serialization, hashes
+
+from cryptography.hazmat.primitives.asymmetric import padding
+
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicKey
+
+from cryptography.hazmat.primitives.asymmetric import ec
+
+from cryptography.hazmat.primitives.asymmetric.ec import EllipticCurvePublicKey
+
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
